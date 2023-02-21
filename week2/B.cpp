5f3c7b451e236dc9425d6464466a975d8abbe3cc
Jane Likinova <likinovae@gmail.com>
2023-02-21 21:47:08 +0300

    shift right

---
+++ week2/B.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+#ifndef N
+#define N 2 // actual size of the array
+#endif
+
+void read_array(int (&a)[N]){
+    for (int i = 0; i < N; i++){
+        cin >> a[i];
+    }
+}
+void print_array(int (&a)[N]){
+    for (int i = 0; i < N; i++){
+        cout << a[i] << " ";
+    }
+}
+void shiftRight(int (&a)[N]){
+    int b[N];
+    b[0] = a[N-1];
+    for(int i = 1; i<N; i = i + 1){
+        b[i] = a [i-1];
+    }
+    for (int i=0; i<N; i++)
+    {
+        a[i] = b[i];
+    }     
+}
+int main() {
+    int a [N];
+    read_array(a);
+    shiftRight(a);
+    print_array(a);
+}
