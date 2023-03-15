e1e68fa6dad1c7ab515cf2095beadb5fcc4abae1
Jane Likinova <likinovae@gmail.com>
2023-03-15 23:59:34 +0300

    reverse array and print it

---
+++ week5/D.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+void input_array(int *ptr, size_t N){
+    for(int i = 0; i < N; i++){
+        cin >> ptr[i];
+    }
+}
+void reverse(int *ptr, size_t N){
+    for (int i = 0; i < N/2; i++){
+        int c = ptr[i];
+        ptr[i] = ptr[N-1-i];
+        ptr[N-1-i] = c;
+    }
+}
+void print(const int *ptr, size_t N){
+    for (int i = 0; i < N; i++){
+        cout << ptr[i];
+    }
+}
+int main() {
+    size_t N = 0;
+    cin >> N;
+    int *ptr = new int [N];
+    input_array(ptr, N);
+    reverse(ptr, N);
+    print(ptr, N);
+    return 0;
+}
